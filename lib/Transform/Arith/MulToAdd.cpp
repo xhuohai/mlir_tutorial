31b6348e37a503d74a785f0bac2cf7f4662c01a8
huochenghai <huochenghai@canaan-creative.com>
2025-12-01 18:13:38 +0800

    mul_to_add

---
+++ lib/Transform/Arith/MulToAdd.cpp
@@ -0,0 +1,83 @@
+#include "MulToAdd.h"
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/IR/PatternMatch.h"
+#include "mlir/Support/LogicalResult.h"
+#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
+#include <cstdint>
+#include <iostream>
+
+namespace mlir {
+namespace tutorial {
+
+using arith::AddIOp;
+using arith::ConstantOp;
+using arith::MulIOp;
+
+// Replace y = C*x with y = C/2*x + C/2*x, when C is a power of 2, otherwise do
+// nothing.
+struct PowerOfTwoExpand : public OpRewritePattern<MulIOp> {
+  PowerOfTwoExpand(mlir::MLIRContext *context)
+      : OpRewritePattern<MulIOp>(context, /*benefit=*/2) {}
+
+  LogicalResult matchAndRewrite(MulIOp op,
+                                PatternRewriter &rewriter) const override {
+    Value lhs = op.getLhs();
+    Value rhs = op.getRhs();
+    auto rhsDefiningOp = rhs.getDefiningOp<arith::ConstantIntOp>();
+    if (!rhsDefiningOp) {
+      return failure();
+    }
+
+    auto value = rhsDefiningOp.value();
+    if ((value & (value - 1))) {
+      return failure();
+    }
+
+    auto newConst = rewriter.create<ConstantOp>(
+        rhsDefiningOp->getLoc(),
+        rewriter.getIntegerAttr(rhs.getType(), value / 2));
+    auto newMul = rewriter.create<arith::MulIOp>(op->getLoc(), lhs, newConst);
+    auto newAdd = rewriter.create<AddIOp>(op->getLoc(), newMul, newMul);
+    rewriter.replaceOp(op, {newAdd});
+    rewriter.eraseOp(rhsDefiningOp);
+
+    return success();
+  }
+};
+
+// Replace y = 9*x with y = 8*x + x
+struct PeelFromMul : public OpRewritePattern<MulIOp> {
+  PeelFromMul(mlir::MLIRContext *context)
+      : OpRewritePattern<MulIOp>(context, /*benefit=*/1) {}
+
+  LogicalResult matchAndRewrite(MulIOp op,
+                                PatternRewriter &rewriter) const override {
+    Value lhs = op.getLhs();
+    Value rhs = op.getRhs();
+    auto rhsDefiningOp = rhs.getDefiningOp<arith::ConstantIntOp>();
+    if (!rhsDefiningOp) {
+      return failure();
+    }
+
+    auto value = rhsDefiningOp.value();
+    auto newConst = rewriter.create<ConstantOp>(
+        rhsDefiningOp->getLoc(),
+        rewriter.getIntegerAttr(rhs.getType(), value - 1));
+    auto newMul = rewriter.create<arith::MulIOp>(op->getLoc(), lhs, newConst);
+    auto newAdd = rewriter.create<AddIOp>(op->getLoc(), newMul, lhs);
+    rewriter.replaceOp(op, {newAdd});
+    rewriter.eraseOp(rhsDefiningOp);
+
+    return success();
+  }
+};
+
+void MulToAddPass::runOnOperation() {
+  mlir::RewritePatternSet patterns(&getContext());
+  patterns.add<PowerOfTwoExpand>(&getContext());
+  patterns.add<PeelFromMul>(&getContext());
+  (void)applyPatternsAndFoldGreedily(getOperation(), std::move(patterns));
+}
+
+} // namespace tutorial
+} // namespace mlir
--- tools/tutorial-opt.cpp
+++ tools/tutorial-opt.cpp
@@ -1,6 +1,6 @@
 #include "../lib/Transform/Affine/AffineFullUnroll.h"
+#include "../lib/Transform/Arith/MulToAdd.h"
 #include "mlir/InitAllDialects.h"
-#include "mlir/Pass/PassManager.h"
 #include "mlir/Pass/PassRegistry.h"
 #include "mlir/Tools/mlir-opt/MlirOptMain.h"
 
@@ -9,7 +9,9 @@ int main(int argc, char **argv) {
     registry.insert<mlir::func::FuncDialect, mlir::affine::AffineDialect>();
 
     mlir::PassRegistration<mlir::tutorial::AffineFullUnrollPass>();
-    mlir::PassRegistration<mlir::tutorial::AffineFullUnrollPassAsPatternRewrite>();
+    mlir::PassRegistration<
+        mlir::tutorial::AffineFullUnrollPassAsPatternRewrite>();
+    mlir::PassRegistration<mlir::tutorial::MulToAddPass>();
 
     return mlir::asMainReturnCode(
         mlir::MlirOptMain(argc, argv, "Tutorial Pass Driver", registry));
