05fe59f01d4ad4c090ad4f4307a4fc58934978cb
huochenghai <huochenghai@canaan-creative.com>
2025-12-01 16:25:55 +0800

    wrapper of unroll

--- lib/Transform/Affine/AffineFullUnroll.cpp
+++ lib/Transform/Affine/AffineFullUnroll.cpp
@@ -1,10 +1,22 @@
 #include "AffineFullUnroll.h"
+#include "mlir/Dialect/Affine/IR/AffineOps.h"
+#include "mlir/Dialect/Affine/LoopUtils.h"
+#include "mlir/Support/LogicalResult.h"
 
 namespace mlir {
 namespace tutorial {
 
+using mlir::affine::AffineForOp;
+using mlir::affine::loopUnrollFull;
+
 void AffineFullUnrollPass::runOnOperation() {
-    return;
+  getOperation()->walk([&](AffineForOp op) {
+    if (failed(loopUnrollFull(op))) {
+      op->emitError("failed to fully unroll affine loop");
+      signalPassFailure();
+	}
+  });
+  return;
 }
 
 } // namespace tutorial
