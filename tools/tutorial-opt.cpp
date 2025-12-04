c3bb695cd0128cd6a38c4d7c705ffe649c8f7f40
huochenghai <huochenghai@canaan-creative.com>
2025-12-04 15:26:46 +0800

    add control-flow-sink

--- tools/tutorial-opt.cpp
+++ tools/tutorial-opt.cpp
@@ -2,18 +2,21 @@
 #include "../lib/Transform/Arith/Passes.h"
 #include "../lib/Dialect/Poly/PolyDialect.h"
 #include "mlir/Dialect/Affine/Passes.h"
+#include "mlir/Dialect/SCF/IR/SCF.h"
 #include "mlir/InitAllDialects.h"
 #include "mlir/InitAllPasses.h"
 // #include "mlir/Pass/PassRegistry.h"
 #include "mlir/Pass/PassRegistry.h"
 #include "mlir/Tools/mlir-opt/MlirOptMain.h"
+#include "mlir/Transforms/Passes.h"
 
 int main(int argc, char **argv) {
     mlir::DialectRegistry registry;
-    registry.insert<mlir::func::FuncDialect, mlir::affine::AffineDialect,
+    registry.insert<mlir::func::FuncDialect, mlir::affine::AffineDialect, mlir::scf::SCFDialect,
                     mlir::tutorial::poly::PolyDialect>();
 
     mlir::affine::registerAffinePasses();
+    mlir::registerControlFlowSinkPass();
     mlir::tutorial::registerArithPasses();
     mlir::tutorial::registerAffinePasses();
 
